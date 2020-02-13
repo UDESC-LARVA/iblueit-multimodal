136afa1f4c44488ae3731f6d77e93e0b7030ad1d
jhonatantcn <42412295+jhonatantcn@users.noreply.github.com>
2020-02-13 18:43:07 -0300

    Criação da scene "TestingPlatform" (Versao02_Release53)
    --> Criação da scene "TestingPlatform" para testes na cinta extensora, esta não gera logs, nem é vista dentro do jogo.
    
    Jogo: I BLUE IT
    Versão: 2
    Release: 53

--- Assets/_Game/Scripts/Plataform/Player/PlayerControl.cs
+++ Assets/_Game/Scripts/Plataform/Player/PlayerControl.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 
 namespace Ibit.Plataform
 {
+    
     public partial class Player
     {
+
+        public float TestSensorValue;// p/ a plataforma de testes
+        public float TestPositionValue;// p/ a plataforma de testes
+        
+        public object outputDoPlayer { get; set; }
         private void PositionOnSerial(string msg)
         {
             if (msg.Length < 1)
@@ -14,19 +20,24 @@ namespace Ibit.Plataform
 
             var sensorValue = Parsers.Float(msg);
 
+
             sensorValue = sensorValue < -Pacient.Loaded.PitacoThreshold || sensorValue > Pacient.Loaded.PitacoThreshold ? sensorValue : 0f;
 
             var peak = sensorValue > 0 ? Pacient.Loaded.CapacitiesPitaco.ExpPeakFlow * 0.3f : -Pacient.Loaded.CapacitiesPitaco.InsPeakFlow;
 
-            var nextPosition = sensorValue * CameraLimits.Boundary / peak;
-            nextPosition = Mathf.Clamp(nextPosition, -CameraLimits.Boundary, CameraLimits.Boundary);
+            var nextPosition = sensorValue * CameraLimits.Boundary / peak; // Ponto crucial do cálculo da posição do blue
+            nextPosition = Mathf.Clamp(nextPosition, -CameraLimits.Boundary, CameraLimits.Boundary);// Ponto crucial do cálculo da posição do blue
 
             //Debug.Log($"NextPosition: {nextPosition}\nPeak: {peak}\nSensorVal: {sensorValue}");
+
+            TestSensorValue = sensorValue; // p/ a plataforma de testes
+            TestPositionValue = nextPosition; // p/ a plataforma de testes
 
             var from = this.transform.position;
             var to = new Vector3(this.transform.position.x, -nextPosition);
 
             this.transform.position = Vector3.Lerp(from, to, Time.deltaTime * 15f);
+            
         }
     }
 }
---
+++ Assets/_Game/Scripts/Plataform/UI/TestingPlatform_PositionBlueRead.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Ibit.Plataform.UI
+{
+    public class TestingPlatform_PositionBlueRead : MonoBehaviour
+    {
+        
+        private Player plr;
+        private void Awake()
+        {
+            plr = FindObjectOfType<Player>();
+        } 
+
+        private void Update()
+        {
+            GetComponent<Text>().text = plr.TestPositionValue.ToString();
+        }
+    }
+}
