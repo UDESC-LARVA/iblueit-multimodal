70e531dc58ea8931e05907855206c6b1e2c3fa03
Jhonatan Thallisson Cabral Néry <42412295+jhonatantcn@users.noreply.github.com>
2020-02-13 21:39:35 -0300

    Criação da scene "TestingPlatform" (Versao02_Release53) (#1)
    --> Criação da scene "TestingPlatform" para testes na cinta extensora, esta não gera logs, nem é vista dentro do jogo.
    
    I BLUE IT v2.53

---
+++ Assets/_Game/Scripts/Plataform/UI/TestingPlatform_SerialRead.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Ibit.Plataform.UI
+{
+    public class TestingPlatform_SerialRead : MonoBehaviour
+    {
+        
+        private Player plr;
+        private void Awake()
+        {
+            plr = FindObjectOfType<Player>();
+        } 
+
+        private void Update()
+        {
+            GetComponent<Text>().text = plr.TestSensorValue.ToString();
+        }
+    }
+}
